998352b39bbe39ba75b7a96a701543d4389bdc51
Njengah <joe@njengah.com>
2023-12-17 17:05:37 +0300

    installed dependencies and ready to start

---
+++ composered.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Composered
+ * Description: WordPress Plugin Boilerplate with Composer Support
+ * Version: 1.0.0
+ * Author: Your Name
+ */
+
+// Load Composer autoloader
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Instantiate your boilerplate plugin class
+new Composered\ComposeredPlugin();
---
+++ inc/ComposeredPlugin.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Composered;
+
+class ComposeredPlugin {
+    public function __construct() {
+        // Your plugin initialization code
+    }
+
+    // Add your boilerplate plugin functionality here
+}
